4ccb3f8b51403ccde0dc721055de1fe452309813
Yu <7kamimura74@gmail.com>
2022-01-22 11:38:22 +0900

    udemy training 66.0:33

--- src/components/udemy/project1/ExpenseItems.js
+++ src/components/udemy/project1/ExpenseItems.js
@@ -2,33 +2,35 @@ import styles from './Expense/ExpenseItem.module.scss';
 import ExpenseItem from './Expense/ExpenseItem';
 import NewExpense from './NewExpense/NewExpense';
 import Expenses from './Expense/Expenses';
+import { useState } from 'react';
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const ExpenseItems = () => {
-  // const [expenses, setExpenses] = useState()
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    expenses.push(expense);
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
   };
 
   return (
