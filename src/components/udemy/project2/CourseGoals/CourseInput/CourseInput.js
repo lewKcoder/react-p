39b4bcc1c8014ea2460152663d042bbc5fefc36d
KamimuraY <y.kamimura@i3design.co.jp>
2022-01-24 16:54:25 +0900

    udemy training section6.done

--- src/components/udemy/project2/CourseGoals/CourseInput/CourseInput.js
+++ src/components/udemy/project2/CourseGoals/CourseInput/CourseInput.js
@@ -5,19 +5,27 @@ import styles from './CourseInput.module.scss';
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
+    if (enteredValue.trim().length > 0) {
+      setIsValid(true);
+    }
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={styles.form_control}>
+      <div className={`${styles.form_control} ${!isValid && styles.isvalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
